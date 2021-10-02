c4027ef883577fa850f9a70504c411c665bda152
kos3l <71730597+kos3l@users.noreply.github.com>
2021-10-02 23:35:02 +0200

    building desktop home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <div id="home">
     <v-container class="wrap">
+      <v-row class="top-row">
+        <v-col></v-col>
+        <v-col class="d-flex justify-center align-end">
+          <div class="category-box">
+            <img :src="brush" alt="" />
+          </div>
+        </v-col>
+        <v-col></v-col>
+      </v-row>
       <v-row>
-        <v-col></v-col>
-        <v-col> </v-col>
-        <v-col></v-col>
+        <v-col class="d-flex justify-center align-center">
+          <div class="call-to-action">
+            <h1>{{ callToAction }}</h1>
+          </div>
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -13,17 +24,44 @@
 <script>
 export default {
   name: "Home",
-  props: ["images"],
+  data() {
+    return {
+      callToAction: "CHOOSE YOUR PRODUCTS OF INTEREST",
+      brush: require("../assets/ferdek.jpg"),
+    };
+  },
 };
 </script>
 <style lang="scss">
 #home {
+  background-image: url("../assets/bg.jpg");
+  background-size: cover;
+  background-position: 50% 10%;
   margin-top: 3.6rem;
   height: 100vh;
 }
 .wrap {
-  background-image: url("/src/assets/bg.jpg");
-  background-size: cover;
-  height: 100vh;
+  height: 100%;
+}
+.category-box {
+  @include container_mixin(
+    3px,
+    map-get($colorz, secondary),
+    0.875rem,
+    0,
+    map-get($colorz, secondary),
+    map-get($colorz, primary)
+  );
+  height: 17rem;
+}
+.category-box img {
+  width: 100%;
+  height: 100%;
+}
+.category-box img {
+  background-color: red;
+}
+.top-row {
+  height: 65%;
 }
 </style>
