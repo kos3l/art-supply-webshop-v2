579ab148f049f23b1af833e5be6721bd76e0d989
kos3l <71730597+kos3l@users.noreply.github.com>
2021-10-05 10:40:34 +0200

    created products

--- src/views/Bundles.vue
+++ src/views/Bundles.vue
@@ -5,6 +5,36 @@
 <script>
 export default {
   name: "Bundles",
+  data() {
+    return {
+      drawing: [
+        {
+          name: "Watercolor Paint kit 14 x 40ml",
+          brand: "Talens Van Gogh",
+          price: 1000,
+          type: "For painters",
+        },
+        {
+          name: "Watercolor Paint Irodori Kobako 6 colors",
+          brand: "Kuretake",
+          price: 200,
+          type: "For painters",
+        },
+        {
+          name: "Drawing Crayons Studio 72 colors",
+          brand: "Derwent",
+          price: 1200,
+          type: "For drawers",
+        },
+        {
+          name: "Oil Pastels Neopastel 96 colors",
+          brand: "Caran d'Ache",
+          price: 1600,
+          type: "For drawers",
+        },
+      ],
+    };
+  },
 };
 </script>
 <style></style>
--- src/views/Drawing.vue
+++ src/views/Drawing.vue
@@ -5,6 +5,36 @@
 <script>
 export default {
   name: "Drawing",
+  data() {
+    return {
+      drawing: [
+        {
+          name: "Technical Drawing Pencils kit",
+          brand: "Koh-i-Noor",
+          price: 60,
+          type: "Pencil",
+        },
+        {
+          name: "Pitt Graphite Matts Pencils",
+          brand: "Faber-Castell",
+          price: 20,
+          type: "Pencil",
+        },
+        {
+          name: "Drawing Crayons Artist kit",
+          brand: "Derwent",
+          price: 140,
+          type: "Crayon",
+        },
+        {
+          name: "Watercolor Crayons Marino kit",
+          brand: "Cretacolor",
+          price: 100,
+          type: "Crayon",
+        },
+      ],
+    };
+  },
 };
 </script>
 <style></style>
