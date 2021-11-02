b7cf68bdb5685d95790303571e90e3b08bf7967a
kos3l <71730597+kos3l@users.noreply.github.com>
2021-11-02 22:48:55 +0100

    building product page

--- src/views/ProductPages/Product.vue
+++ src/views/ProductPages/Product.vue
@@ -3,13 +3,30 @@
     <v-container fluid light class="pa-16" style="margin-top: 3.6rem;">
       <v-row class="background-box">
         <v-col class="pa-16">
-          <div class="window-img"></div>
+          <img :src="placeholder" class="product-image" />
         </v-col>
-        <v-col class="pa-16 o">
-          <div v-if="getItem" class="loading">
-            <p>{{ this.getItem.name }}</p>
-            <p>{{ this.getItem.brand }}</p>
-            <p>{{ this.getItem.price }}</p>
+        <v-col class="pa-16 info-column">
+          <div v-if="getItem" class="detailsContainter">
+            <h3 class="text-uppercase">{{ this.getItem.name }}</h3>
+            <div class="line mb-lg-6"></div>
+            <div class=" d-flex justify-space-between mb-lg-3 ">
+              <p class="font-weight-bold">BRAND:</p>
+              <p>{{ this.getItem.brand }}</p>
+            </div>
+            <div class=" d-flex justify-space-between mb-3">
+              <p class="font-weight-bold">PRICE:</p>
+              <p>{{ this.getItem.price }}</p>
+            </div>
+            <div class=" d-flex flex-column mb-3">
+              <p class="font-weight-bold mb-1">DESCRIPTION:</p>
+              <p class="describ">{{ this.getItem.description }}</p>
+            </div>
+            <div class="add-to-cart d-flex align-end justify-end">
+              <button class="addCartButton">
+                <p>ADD TO CART</p>
+              </button>
+            </div>
+            <!-- add description and color if u have the time-->
           </div>
         </v-col>
       </v-row>
@@ -29,6 +46,7 @@ export default {
   data() {
     return {
       post: "",
+      placeholder: require("../../assets/lefrancOilPaint.jpeg"),
     };
   },
 
@@ -59,7 +77,49 @@ export default {
 .background-box {
   background-color: map-get($colorz, secondary);
 }
-.o p {
-  color: white;
+.info-column p,
+.info-column h3 {
+  color: map-get($colorz, primary);
+}
+.describ {
+  letter-spacing: 0;
+}
+.info-column h3 {
+  margin-bottom: 0.5rem;
+}
+.detailsContainter {
+  width: 100%;
+  height: 100%;
+}
+.product-image {
+  width: 100%;
+  height: 100%;
+}
+.add-to-cart {
+  width: 100%;
+  height: auto;
+}
+.add-to-cart button {
+  width: 100%;
+}
+.addCartButton {
+  @include container_mixin(
+    2px,
+    map-get($colorz, primary),
+    0,
+    0,
+    map-get($colorz, primary),
+    map-get($colorz, secondary)
+  );
+
+  height: 50px;
+  border-radius: 30px;
+  box-shadow: 2px 2px 8px map-get($colorz, highlight);
+}
+.addCartButton p {
+  margin: 0;
+  padding: 0;
+  font-weight: 700;
+  font-size: 1.2rem;
 }
 </style>
