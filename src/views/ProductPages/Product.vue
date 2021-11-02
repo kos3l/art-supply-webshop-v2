b3ac5ae3bad9bbcaa06612a9567fc05cdae57974
kos3l <71730597+kos3l@users.noreply.github.com>
2021-11-02 22:55:14 +0100

    prepared layout of product page

--- src/views/ProductPages/Product.vue
+++ src/views/ProductPages/Product.vue
@@ -5,21 +5,26 @@
         <v-col class="pa-16">
           <img :src="placeholder" class="product-image" />
         </v-col>
-        <v-col class="pa-16 info-column">
-          <div v-if="getItem" class="detailsContainter">
-            <h3 class="text-uppercase">{{ this.getItem.name }}</h3>
-            <div class="line mb-lg-6"></div>
-            <div class=" d-flex justify-space-between mb-lg-3 ">
-              <p class="font-weight-bold">BRAND:</p>
-              <p>{{ this.getItem.brand }}</p>
-            </div>
-            <div class=" d-flex justify-space-between mb-3">
-              <p class="font-weight-bold">PRICE:</p>
-              <p>{{ this.getItem.price }}</p>
-            </div>
-            <div class=" d-flex flex-column mb-3">
-              <p class="font-weight-bold mb-1">DESCRIPTION:</p>
-              <p class="describ">{{ this.getItem.description }}</p>
+        <v-col class="pa-16 pl-0 info-column">
+          <div
+            v-if="getItem"
+            class="detailsContainter d-flex flex-column justify-space-around"
+          >
+            <div>
+              <h3 class="text-uppercase">{{ this.getItem.name }}</h3>
+              <div class="line mb-lg-6"></div>
+              <div class=" d-flex justify-space-between mb-lg-3 ">
+                <p class="font-weight-bold">BRAND:</p>
+                <p>{{ this.getItem.brand }}</p>
+              </div>
+              <div class=" d-flex justify-space-between mb-3">
+                <p class="font-weight-bold">PRICE:</p>
+                <p>{{ this.getItem.price }} DKK</p>
+              </div>
+              <div class=" d-flex flex-column mb-3">
+                <p class="font-weight-bold mb-1">DESCRIPTION:</p>
+                <p class="describ">{{ this.getItem.description }}</p>
+              </div>
             </div>
             <div class="add-to-cart d-flex align-end justify-end">
               <button class="addCartButton">
@@ -100,7 +105,7 @@ export default {
   height: auto;
 }
 .add-to-cart button {
-  width: 100%;
+  width: 50%;
 }
 .addCartButton {
   @include container_mixin(
@@ -112,7 +117,7 @@ export default {
     map-get($colorz, secondary)
   );
 
-  height: 50px;
+  height: 4rem;
   border-radius: 30px;
   box-shadow: 2px 2px 8px map-get($colorz, highlight);
 }
