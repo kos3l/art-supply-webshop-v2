0ed058ed3e509ba8e5b67ffe46d45a56f8789914
kos3l <71730597+kos3l@users.noreply.github.com>
2021-11-07 14:11:01 +0100

    working on add new item

---
+++ src/components/admin/AddNewItems.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="product-page">
+    <v-container fluid light class="pa-0">
+      <v-row>
+        <v-col class="pa-10 pb-0">
+          <div class="line-dark"></div>
+        </v-col>
+      </v-row>
+      <v-row class="pa-10 pt-5 pl-7">
+        <v-col>
+          <div class="add-container">
+            <div class="add-banner dark d-flex justify-center align-center">
+              <h3>ADD NEW ITEM</h3>
+            </div>
+            <div class="fields-container">
+              <div class="name">
+                <input type="text" placeholder="Name" v-model="name" />
+              </div>
+              <div class="brand">
+                <input type="text" placeholder="Brand" v-model="brand" />
+              </div>
+              <div class="prices">
+                <input type="text" placeholder="Price" v-model="price" />
+              </div>
+              <div class="category">
+                <input type="text" placeholder="Category" v-model="category" />
+              </div>
+              <div class="type">
+                <input type="text" placeholder="Type" v-model="type" />
+              </div>
+              <div class="description">
+                <input
+                  type="text"
+                  placeholder="Description"
+                  v-model="description"
+                />
+              </div>
+            </div>
+          </div>
+        </v-col>
+        <v-col>
+          <div class="add-container">
+            <div class="add-banner dark d-flex justify-center align-center">
+              <h3>PREVIEW</h3>
+            </div>
+            <div class="preview-container d-flex">
+              <div class="admin-item-info">
+                <h3><span id="name-span"> name:</span> {{ name }}</h3>
+                <span id="name-span">brand:</span> {{ brand }} /<span
+                  id="name-span"
+                >
+                  price:</span
+                >
+                {{ price }} DKK /
+                <span id="name-span"> category: {{ category }} </span> /
+                <span id="name-span"> type: {{ type }} </span>
+                <br />
+                <p>
+                  <span id="name-span">description: </span>{{ description }}
+                </p>
+              </div>
+              <div class="admin-item-img">
+                img
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddNewItems",
+  data() {
+    return {
+      name: "",
+      brand: "",
+      price: "",
+      description: "",
+      category: "",
+      type: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.add-container {
+  @include container_mixin(
+    2px,
+    map-get($colorz, secondary),
+    0,
+    3rem,
+    map-get($colorz, secondary),
+    map-get($colorz, primary)
+  );
+  height: auto;
+}
+.add-banner {
+  border-bottom: 2px solid map-get($colorz, secondary);
+  height: 4rem;
+}
+#name-span {
+  font-weight: normal;
+  font-style: italic;
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,77 +2,191 @@
   <div class="product-page">
     <v-container fluid light class="pa-0">
       <v-row>
-        <v-col class="pa-10">
+        <v-col class="pa-10 pb-0">
           <div class="line-dark"></div>
-          <h2 class="mt-5 welcome-msg">
-            Welcome at your Admin page
-          </h2>
-          <h3 class="font-weight-regular">
-            Here you can preview your products and add / edit / delete them!
-          </h3>
         </v-col>
       </v-row>
-      <v-row class="pa-10">
+      <v-row class="pa-10 pt-5 pl-7">
         <v-col xl="8">
           <v-col class="editContainer">
             <div
-              class="name-banner d-flex justify-space-between align-center px-5"
+              class="name-banner dark d-flex justify-space-between align-center px-5"
             >
               <h2>PAINTING</h2>
               <v-btn icon>
-                <v-icon v-on:click="isHiddenType = !isHiddenType">
+                <v-icon large color="primary" v-on:click="isHidden = !isHidden">
                   mdi-chevron-down
                 </v-icon>
               </v-btn>
             </div>
-            <div
-              class="dropDown-container pa-5"
-              v-for="paintingItem in paintingItems"
-              :key="paintingItem.id"
-            >
-              <div>
-                {{ paintingItem.name }}
-                {{ paintingItem.brand }}
-                {{ paintingItem.price }}
-                {{ paintingItem.category }}
-                {{ paintingItem.type }}
-                {{ paintingItem.description }}
-              </div>
-              <div>
-                img
+            <div v-if="isHidden">
+              <div
+                class="dropDown-container pa-5 d-flex"
+                v-for="paintingItem in paintingItems"
+                :key="paintingItem.id"
+              >
+                <div class="admin-item-info">
+                  <h3>{{ paintingItem.name }}</h3>
+                  {{ paintingItem.brand }} / {{ paintingItem.price }} DKK /
+                  {{ paintingItem.category }} / {{ paintingItem.type }}
+                  <br />
+                  <p>{{ paintingItem.description }}</p>
+                  <div class="d-flex align-center options">
+                    <p class="pr-3 font-weight-medium" style="font-size:1.1rem">
+                      EDIT:
+                    </p>
+                    <v-btn icon>
+                      <v-icon color="highlight">
+                        mdi-pencil
+                      </v-icon>
+                    </v-btn>
+                    <p
+                      class="pr-3 ml-10 font-weight-medium"
+                      style="font-size:1.1rem"
+                    >
+                      DELETE:
+                    </p>
+                    <v-btn icon>
+                      <v-icon color="highlight">
+                        mdi-delete
+                      </v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+                <div class="admin-item-img">
+                  img
+                </div>
               </div>
             </div>
           </v-col>
 
           <v-col class="editContainer">
             <div
-              class="name-banner d-flex justify-space-between align-center px-5"
+              class="name-banner dark d-flex justify-space-between align-center px-5"
             >
               <h2>DRAWING</h2>
               <v-btn icon>
-                <v-icon v-on:click="isHiddenType = !isHiddenType">
+                <v-icon
+                  large
+                  color="primary"
+                  v-on:click="isHiddenDrawing = !isHiddenDrawing"
+                >
                   mdi-chevron-down
                 </v-icon>
               </v-btn>
+            </div>
+            <div v-if="isHiddenDrawing">
+              <div
+                class="dropDown-container pa-5 d-flex"
+                v-for="drawingItem in drawingItems"
+                :key="drawingItem.id"
+              >
+                <div class="admin-item-info">
+                  <h3>{{ drawingItem.name }}</h3>
+                  {{ drawingItem.brand }} / {{ drawingItem.price }} DKK /
+                  {{ drawingItem.category }}
+                  <br />
+                  <p>{{ drawingItem.description }}</p>
+                  <div class="d-flex align-center options">
+                    <p class="pr-3 font-weight-medium" style="font-size:1.1rem">
+                      EDIT:
+                    </p>
+                    <v-btn icon>
+                      <v-icon color="highlight">
+                        mdi-pencil
+                      </v-icon>
+                    </v-btn>
+                    <p
+                      class="pr-3 ml-10 font-weight-medium"
+                      style="font-size:1.1rem"
+                    >
+                      DELETE:
+                    </p>
+                    <v-btn icon>
+                      <v-icon color="highlight">
+                        mdi-delete
+                      </v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+                <div class="admin-item-img">
+                  img
+                </div>
+              </div>
             </div>
           </v-col>
           <v-col class="editContainer">
             <div
-              class="name-banner d-flex justify-space-between align-center px-5"
+              class="name-banner dark d-flex justify-space-between align-center px-5"
             >
               <h2>BUNDLES</h2>
               <v-btn icon>
-                <v-icon v-on:click="isHiddenType = !isHiddenType">
+                <v-icon
+                  large
+                  color="primary"
+                  v-on:click="isHiddenBundles = !isHiddenBundles"
+                >
                   mdi-chevron-down
                 </v-icon>
               </v-btn>
             </div>
+            <div v-if="isHiddenBundles">
+              <div
+                class="dropDown-container pa-5 d-flex"
+                v-for="bundlesItem in bundlesItems"
+                :key="bundlesItem.id"
+              >
+                <div class="admin-item-info">
+                  <h3>{{ bundlesItem.name }}</h3>
+                  {{ bundlesItem.brand }} / {{ bundlesItem.price }} DKK /
+                  {{ bundlesItem.category }}
+                  <br />
+                  <p>{{ bundlesItem.description }}</p>
+                  <div class="d-flex align-center options">
+                    <p class="pr-3 font-weight-medium" style="font-size:1.1rem">
+                      EDIT:
+                    </p>
+                    <v-btn icon>
+                      <v-icon color="highlight">
+                        mdi-pencil
+                      </v-icon>
+                    </v-btn>
+                    <p
+                      class="pr-3 ml-10 font-weight-medium"
+                      style="font-size:1.1rem"
+                    >
+                      DELETE:
+                    </p>
+                    <v-btn icon>
+                      <v-icon color="highlight">
+                        mdi-delete
+                      </v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+                <div class="admin-item-img">
+                  img
+                </div>
+              </div>
+            </div>
           </v-col>
         </v-col>
-        <v-col xl="4">
-          <p>
-            add items
-          </p>
+        <v-col xl="4" class="pl-10">
+          <h1 class="mt-5 welcome-msg">
+            Welcome at your Admin page
+          </h1>
+          <h3 class="font-weight-regular">
+            Here you can preview your products and add / edit / delete them!
+            Remember when you create a new object to select in which main
+            category you would like it to be, like painting, drawing or bundles.
+          </h3>
+          <div class="d-flex justify-center">
+            <router-link :to="{ name: 'addNew' }">
+              <button class="add mr-xl-8 mt-14 ">
+                <p>ADD NEW</p>
+              </button>
+            </router-link>
+          </div>
         </v-col>
       </v-row>
     </v-container>
@@ -81,18 +195,29 @@
 
 <script>
 export default {
+  name: "Admin",
   data() {
     return {
-      isHidden: true,
+      isHidden: false,
+      isHiddenBundles: false,
+      isHiddenDrawing: false,
     };
   },
   beforeCreate() {
     this.$store.dispatch("setPaintingItemsAction");
+    this.$store.dispatch("setDrawingItemsAction");
+    this.$store.dispatch("setBundlesItemsAction");
   },
 
   computed: {
     paintingItems() {
       return this.$store.getters.getPaintingItems;
+    },
+    bundlesItems() {
+      return this.$store.getters.getBundlesItems;
+    },
+    drawingItems() {
+      return this.$store.getters.getDrawingItems;
     },
   },
 };
@@ -118,8 +243,46 @@ export default {
   width: 100%;
   height: 4rem;
 }
+.name-banner button {
+  color: white;
+}
 .dropDown-container {
   border-top: 2px solid map-get($colorz, secondary);
   height: auto;
+}
+.admin-item-info {
+  width: 60%;
+}
+.admin-item-img {
+  width: 40%;
+  background-color: red;
+}
+.admin-item-info p {
+  letter-spacing: 0;
+  padding-right: 2rem;
+  margin-top: 1rem;
+}
+.welcome-msg {
+  text-shadow: 0px 1px 8px #7f7f88;
+}
+.add {
+  @include container_mixin(
+    2px,
+    map-get($colorz, secondary),
+    0,
+    0,
+    map-get($colorz, secondary),
+    map-get($colorz, primary)
+  );
+  width: 300px;
+  height: 60px;
+  border-radius: 30px;
+  box-shadow: 2px 2px 8px map-get($colorz, highlight);
+}
+.add p {
+  margin: 0;
+  padding: 0;
+  font-weight: 900;
+  font-size: 1.2rem;
 }
 </style>
